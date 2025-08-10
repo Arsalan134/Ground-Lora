3948381608647b2701d8106eaf874e7523fec943
Arsalan Iravani <airavani2018@gmail.com>
2025-08-10 21:41:01 +0200

    Claude

--- src/Airplane.cpp
+++
@@ -1,9 +0,0 @@
-// byte trim = 90;
-// #define trimStep 2
-
-// unsigned long lastRecievedTime = millis();
-
-// bool emergencyStopIsActive = true;
-
-// #define minAnalogReadFromBattery 750
-// #define maxAnalogReadFromBattery 1000
